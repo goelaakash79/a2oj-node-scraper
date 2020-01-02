e744aed48db782a548235ae33236ddb63263c6bb
Aakash Goel <goelaakash79@gmail.com>
2020-01-03 03:26:34 +0530

    first commit

--- controllers/index_controller.js
+++ controllers/index_controller.js
@@ -1,5 +1,52 @@
 require("dotenv").config();
+const puppeteer = require("puppeteer");
+const fs = require("fs");
 
 module.exports.index = (req, res) => {
-  return res.status(200).json({ message: "" });
+  let a2ojUrl = "https://www.a2oj.com/Ladder10.html";
+  (async () => {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1920, height: 926 });
+    await page.goto(a2ojUrl);
+
+    // get div details
+    let divData = await page.evaluate(() => {
+      let questions = [];
+      // get the question elements
+      let elements = document.querySelectorAll(
+        "body > center > table:nth-child(3) > tbody > tr"
+      );
+      // get the question data
+      elements.forEach(el => {
+        let quesJson = {};
+        try {
+          quesJson.id = el.querySelector(":nth-child(1)").innerText;
+          quesJson.name = el.querySelector("td:nth-child(2) > a").innerText;
+          quesJson.link = el
+            .querySelector("td:nth-child(2) > a")
+            .getAttribute("href");
+          quesJson.judge = el.querySelector("td:nth-child(3)").innerText;
+          quesJson.level = el.querySelector("td:nth-child(4)").innerText;
+          if (el.querySelector("strong.price")) {
+            quesJson.price = el.querySelector("strong.price").innerText;
+          }
+        } catch (exception) {}
+        questions.push(quesJson);
+      });
+      return questions;
+    });
+
+    console.dir(divData);
+    const data = divData;
+
+    fs.writeFile("./div1e.json", JSON.stringify(data), err => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log("File has been created");
+      return;
+    });
+  })();
 };
--- index.js
+++ index.js
@@ -1,40 +1,25 @@
 const express = require("express");
-const path = require('path');
-const bodyParser = require('body-parser');
+const path = require("path");
 const morgan = require("morgan");
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
 
-require('dotenv').config();
-require('./config/dbconnection');
+const { index } = require("./controllers/index_controller");
+require("dotenv").config();
+// require('./config/dbconnection');
 
-app.use(cors());
-app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(morgan('tiny'));
-app.use(express.urlencoded({extended: true}));
-app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// load schemas
-const User = require("./models/User");
+// app.use(cors());
+app.use(morgan("dev"));
 
 // Routes
-app.use('/api/v1/abc', require('./routes/index'));
+app.get("/", index);
 
-// app.use(express.static(path.join(__dirname, 'client/build')))
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname + '/client/build/index.html'))
-// })
-
-// app.get('*', (req, res) => {
-//     res.render('notfound');
-// });
-
-app.listen(process.env.PORT, (err) => {
-    if (err) {
-        console.log("Error in running server");
-        return;
-    }
-    console.log(`Server is up and running on http://localhost:${process.env.PORT}`);
+app.listen(process.env.PORT, err => {
+  if (err) {
+    console.log("Error in running server");
+    return;
+  }
+  console.log(
+    `Server is up and running on http://localhost:${process.env.PORT}`
+  );
 });
